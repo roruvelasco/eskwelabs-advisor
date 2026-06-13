82d8d74913d9e0c163e3b573868f093e82d5f4ed
russvelasco <velascoronrussell@gmail.com>
2026-06-13 12:24:21 +0800

    dfsdf

--- packages/ui/src/components/ui/sidebar.tsx
+++ packages/ui/src/components/ui/sidebar.tsx
@@ -175,11 +175,10 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="bg-sidebar text-sidebar-foreground p-0 [&>button]:hidden"
+          className="bg-sidebar text-sidebar-foreground w-(--sidebar-width) max-w-(--sidebar-width) sm:max-w-(--sidebar-width) p-0 [&>button]:hidden"
           style={
             {
-              '--sidebar-width': SIDEBAR_WIDTH_MOBILE,
-              width: '70vw'
+              '--sidebar-width': SIDEBAR_WIDTH_MOBILE
             } as React.CSSProperties
           }
           side={side}
