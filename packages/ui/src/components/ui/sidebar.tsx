d31d5643cecfdbe59f183e590b526a779295afc9
russvelasco <velascoronrussell@gmail.com>
2026-06-13 12:19:58 +0800

    make sidebar 70% expanded on mobile:

--- packages/ui/src/components/ui/sidebar.tsx
+++ packages/ui/src/components/ui/sidebar.tsx
@@ -175,10 +175,11 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="w-(--sidebar-width) bg-sidebar text-sidebar-foreground p-0 [&>button]:hidden"
+          className="bg-sidebar text-sidebar-foreground p-0 [&>button]:hidden"
           style={
             {
-              '--sidebar-width': SIDEBAR_WIDTH_MOBILE
+              '--sidebar-width': SIDEBAR_WIDTH_MOBILE,
+              width: '70vw'
             } as React.CSSProperties
           }
           side={side}
