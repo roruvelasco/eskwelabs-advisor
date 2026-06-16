14026f392c523d840f0b69d52b8aeaf9d295147b
russvelasco <velascoronrussell@gmail.com>
2026-06-16 12:32:54 +0800

    fix: add eslint-plugin-react-hooks to fix Vercel build

--- packages/apps-config/eslint.js
+++ packages/apps-config/eslint.js
@@ -1,11 +1,19 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: { 'react-hooks': reactHooks },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  },
   prettier,
   {
     ignores: [
